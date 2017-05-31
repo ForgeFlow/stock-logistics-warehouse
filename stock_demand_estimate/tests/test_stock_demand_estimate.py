a44635fec825fe5d299fb70697e3adf6147060d8
antonio <espo-tony@hotmail.it>
2017-05-31 11:24:18 +0200

    [FIX] Some flake8 adjustments

--- stock_demand_estimate/tests/test_stock_demand_estimate.py
+++ stock_demand_estimate/tests/test_stock_demand_estimate.py
@@ -7,8 +7,8 @@ from dateutil.rrule import MONTHLY
 
 
 class TestStockDemandEstimate(common.TransactionCase):
-
     def setUp(self):
+        
         super(TestStockDemandEstimate, self).setUp()
         self.res_users_model = self.env['res.users']
         self.product_model = self.env['product.product']
@@ -78,33 +78,54 @@ class TestStockDemandEstimate(common.TransactionCase):
         sheets = self.env['stock.demand.estimate.sheet'].search([])
         for sheet in sheets:
 
-            self.assertEquals(len(sheet.line_ids), 12, 'There should be 12 '
-                                                       'lines.')
-            self.assertEquals(sheet.date_start, '1943-01-01', 'The date start '
-                                                         'should be 1943-01-01')
-            self.assertEquals(sheet.date_end, '1943-12-31', 'The date end '
-                                                            'should be '
-                                                            '1943-12-31')
-            self.assertEquals(sheet.location_id.id, self.location.id,
-                              'Wrong location')
-            self.assertEquals(sheet.product_ids.ids, [self.product1.id],
-                              'Wrong products')
+            self.assertEquals(
+                len(sheet.line_ids),
+                12,
+                'There should be 12 lines.')
+            self.assertEquals(
+                sheet.date_start,
+                '1943-01-01',
+                'The date start should be 1943-01-01')
+            self.assertEquals(
+                sheet.date_end,
+                '1943-12-31',
+                'The date end should be 1943-12-31')
+            self.assertEquals(
+                sheet.location_id.id,
+                self.location.id,
+                'Wrong location')
+            self.assertEquals(
+                sheet.product_ids.ids,
+                [self.product1.id],
+                'Wrong products')
+
             for line in sheet.line_ids:
                 line.product_uom_qty = 1
-                self.assertEquals(line.product_id.id, self.product1.id,
-                                  'The product does not match in the line')
-                self.assertEquals(line.location_id.id, self.location.id,
-                                  'The product does not match in the line')
+                self.assertEquals(
+                    line.product_id.id,
+                    self.product1.id,
+                    'The product does not match in the line')
+                self.assertEquals(
+                    line.location_id.id,
+                    self.location.id,
+                    'The product does not match in the line')
             sheet.button_validate()
             estimates = self.env['stock.demand.estimate'].search([(
                 'date_range_type_id', '=', self.drt_monthly)])
-            self.assertEquals(len(estimates), 12, 'There should be 12 '
-                                                  'estimate records.')
+            self.assertEquals(
+                len(estimates),
+                12,
+                'There should be 12 estimate records.')
+
             for estimate in estimates:
-                self.assertEquals(estimate.product_id.id, self.product1.id,
-                                  'The product does not match in the estimate')
-                self.assertEquals(estimate.location_id.id, self.location.id,
-                                  'The product does not match in the estimate')
+                self.assertEquals(
+                    estimate.product_id.id,
+                    self.product1.id,
+                    'The product does not match in the estimate')
+                self.assertEquals(
+                    estimate.location_id.id,
+                    self.location.id,
+                    'The product does not match in the estimate')
 
         sheets = self.env['stock.demand.estimate.sheet'].search([])
         for sheet in sheets:
@@ -120,5 +141,7 @@ class TestStockDemandEstimate(common.TransactionCase):
         sheets = self.env['stock.demand.estimate.sheet'].search([])
         for sheet in sheets:
             for line in sheet.line_ids:
-                self.assertEquals(line.product_uom_qty,1,
-                                  'The quantity should be 1')
+                self.assertEquals(
+                    line.product_uom_qty,
+                    1,
+                    'The quantity should be 1')
