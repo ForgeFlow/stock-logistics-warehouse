ffa873208a040d642ab1ca8064edab2feed4a039
mariadforgeflow <maria.de.luna@forgeflow.com>
2023-05-23 16:20:15 +0200

    [ADD] stock_reserve_area

---
+++ stock_reserve_area/hooks.py
@@ -0,0 +1,42 @@
+# Copyright 2023 ForgeFlow SL.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import SUPERUSER_ID, api
+from odoo.tools.sql import column_exists
+
+
+def pre_init_hook(cr):
+    if not column_exists(cr, "stock_move", "area_reserved_availability"):
+        cr.execute(
+            """
+                ALTER TABLE "stock_move"
+                ADD COLUMN "area_reserved_availability" double precision DEFAULT 0
+            """
+        )
+        cr.execute(
+            """
+        ALTER TABLE "stock_move" ALTER COLUMN "area_reserved_availability" DROP DEFAULT
+        """
+        )
+
+
+def post_init_hook(cr, registry):
+    """
+    This post-init-hook will create a Reserve Area for each existing WH.
+    """
+    env = api.Environment(cr, SUPERUSER_ID, dict())
+    warehouse_obj = env["stock.warehouse"]
+    warehouses = warehouse_obj.search([])
+    reserve_area_obj = env["stock.reserve.area"]
+    for warehouse_id in warehouses.ids:
+        warehouse = warehouse_obj.browse(warehouse_id)
+        all_locations = env["stock.location"].search(
+            [("id", "child_of", warehouse.view_location_id.id)]
+        )
+        reserve_area_obj.create(
+            {
+                "name": warehouse.name,
+                "location_ids": [(6, 0, all_locations.ids)],
+                "company_id": warehouse.company_id.id,
+            }
+        )
