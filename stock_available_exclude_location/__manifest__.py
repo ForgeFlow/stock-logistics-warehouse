20706e6d76f31405c453cde791973eede396cf20
AaronHForgeFlow <aaron.henriquez@forgeflow.com>
2025-01-15 17:30:43 +0100

    [MIG] stock_available_exclude_location: Migration to v16

--- stock_available_exclude_location/__manifest__.py
+++ stock_available_exclude_location/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Stock Available Exclude Location",
     "summary": """
         Exclude locations for product available quantities""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Stock",
     "license": "AGPL-3",
     "author": "Escodoo,Odoo Community Association (OCA)",
