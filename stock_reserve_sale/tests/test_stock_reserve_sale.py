15e9db030938ff3b2e71d964e6f633d637955edd
AaronHForgeFlow <aaron.henriquez@forgeflow.com>
2025-09-26 17:36:10 +0200

    [MIG] stock_reserve_sale: Migration to v15

--- stock_reserve_sale/tests/test_stock_reserve_sale.py
+++ stock_reserve_sale/tests/test_stock_reserve_sale.py
@@ -1,4 +1,5 @@
 # Copyright 2021 Tecnativa - Carlos Roca
+# Copyright 2023 Michael Tietz (MT Software) <mtietz@mt-software.de>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo.exceptions import UserError
 from odoo.tests import Form, common
@@ -136,3 +137,12 @@ class TestStockReserveSale(common.SavepointCase):
             so.unlink()
         with self.assertRaises(UserError):
             so.order_line.unlink()
+
+    def test_reserve_06_all(self):
+        sale_order_form = Form(self.env["sale.order"])
+        sale_order_form.partner_id = self.partner
+        with sale_order_form.order_line.new() as order_line_form:
+            order_line_form.product_id = self.product_1
+            order_line_form.product_uom_qty = 3
+        so = sale_order_form.save()
+        so.action_reserve_all_lines()
